afdefc633a8952eb05b0861d3d894110b581b875
jayalodha <jayalodha@gmail.com>
2020-02-05 15:05:24 +0000

    UI update to Detail page

--- app/src/main/java/com/rushikeshkardetest/ui/EmployeeDetailsActivity.java
+++ app/src/main/java/com/rushikeshkardetest/ui/EmployeeDetailsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.rushikeshkardetest.R;
 import com.rushikeshkardetest.model.Result;
 import com.rushikeshkardetest.utils.Constants;
@@ -17,11 +19,11 @@ import java.util.ArrayList;
 public class EmployeeDetailsActivity extends AppCompatActivity {
     TextView tvFirstName;
     TextView tvLastName;
-    ImageView imageView;
+    ImageView ivProfile;
     TextView tvDOB;
     TextView tvLocation;
     TextView tvEmail;
-    TextView PhoneNumber;
+    TextView tvPhoneNumber;
 
 
     ArrayList<Result> employees;
@@ -35,10 +37,36 @@ public class EmployeeDetailsActivity extends AppCompatActivity {
         {
             employees = new ArrayList<>();
             getIntent().getSerializableExtra(Constants.KEY_PASS_DATA);
+
+            tvFirstName.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_FIRST_NAME)));
+            tvLastName.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_LAST_NAME)));
+            tvDOB.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_EMAIL)));
+            tvLocation.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_LOCATION)));
+            tvEmail.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_EMAIL)));
+            tvPhoneNumber.setText(String.valueOf(getIntent().getStringExtra(Constants.KEY_PHONE_NUMBER)));
+            setProfileImage(getIntent().getStringExtra(Constants.KEY_IMAGE_URL));
         }
+    }
+
+    private void setProfileImage(String imageURL) {
+        Glide.with(ivProfile.getContext())
+                .setDefaultRequestOptions(new RequestOptions()
+                        .circleCrop())
+                .load(imageURL)
+                .placeholder(R.drawable.loading)
+                .into(ivProfile);
     }
 
     private void initUI() {
         tvFirstName = (TextView) findViewById(R.id.tv_first_name);
+        tvLastName = (TextView) findViewById(R.id.tv_last_name);
+        tvDOB = (TextView) findViewById(R.id.tv_dob);
+        tvLocation = (TextView) findViewById(R.id.tv_location);
+        tvEmail= (TextView) findViewById(R.id.tv_email);
+        tvPhoneNumber= (TextView) findViewById(R.id.tv_phone_number);
+        ivProfile =(ImageView) findViewById(R.id.iv_profile_pic);
+
     }
+
+
 }
